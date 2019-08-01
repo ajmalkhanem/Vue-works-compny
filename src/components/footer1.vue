924fced55f850e5407a7666b93fa2ac2874aaa5a
John Doe <shimijoseph94@gmail.com>
2019-08-01 12:12:02 +0530

    new

--- src/components/footer1.vue
+++ src/components/footer1.vue
@@ -1,65 +1,107 @@
 <template>
     <div>
-            <b-jumbotron>
-            
-                        
-          <b-row>
-              <b-col>
+        <b-jumbotron>
+
+
+            <b-row>
+                <b-col style="padding-left: 100px;">
                     <h5><b>Cloud Online global market</b></h5>
                     <p>Cloud Global Market ,</p>
                     <p>United Arab Emirates</p>
-                   <p>Contact No : +971501615167 </p>
-                <p>Land line : 072535264</p>
-                <p> admin@cloudgm.com</p>
-                <br>
-              </b-col>
-              
-            <b-col>
-                <h5><b>Categories</b></h5>
-                <br>
-                <router-link to=""><h6>Automobile</h6></router-link>
-                <router-link to=""><h6>Communication</h6></router-link>
-                <router-link to=""><h6>Education</h6></router-link>
-                <router-link to=""><h6>Entertainment & Sports</h6></router-link>
-                <router-link to=""><h6>Financial Solutions</h6></router-link>
-                <router-link to=""><h6>Health & Wellness</h6></router-link>
-                <router-link to=""><h6>Insurance</h6></router-link>
-
-            </b-col>
-            <b-col>
-                    <h5><b>About</b></h5>
+                    <p>Contact No : +971501615167 </p>
+                    <p>Land line : 072535264</p>
+                    <p> admin@cloudgm.com</p>
                     <br>
-                    <router-link to=""><h6>Careers</h6></router-link>
-                    <router-link to=""><h6>Press & News</h6></router-link>
-                    <router-link to=""><h6>Partnerships</h6></router-link>
-                    <router-link to=""><h6>Privacy policy</h6></router-link>
-                    <router-link to=""><h6>Terms of service</h6></router-link>
-                   
-    
+                </b-col>
+
+                <b-col>
+                    <h5><b>Categories</b></h5>
+                    <br>
+                    <router-link to="">
+                        <h6>Automobile</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Communication</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Education</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Entertainment & Sports</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Financial Solutions</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Health & Wellness</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Insurance</h6>
+                    </router-link>
+
                 </b-col>
                 <b-col>
-                        <h5><b>Support</b></h5>
-                        <br>
-                        <router-link to=""><h6>Help & Support</h6></router-link>
-                        <router-link to=""><h6>Trust & Safety</h6></router-link>
-                        <router-link to=""><h6>Selling on CloudGM</h6></router-link>
-                        <router-link to=""><h6>Buying On Cloud GM</h6></router-link>
-                       
-        
-                    </b-col>
-                    <b-col>
-                            <h5><b>Community</b></h5>
-                            <br>
-                            <router-link to=""><h6>Events</h6></router-link>
-                            <router-link to=""><h6>Forums</h6></router-link>
-                            <router-link to=""><h6>Podcasts</h6></router-link>
-                            <router-link to=""><h6>Invite Friends</h6></router-link>
-                            <router-link to=""><h6>Become Seller</h6></router-link>
-                            
-            
-                        </b-col>
-          </b-row>
-        
-                </b-jumbotron>
+                    <h5><b>About</b></h5>
+                    <br>
+                    <router-link to="">
+                        <h6>Careers</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Press & News</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Partnerships</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Privacy policy</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Terms of service</h6>
+                    </router-link>
+
+
+                </b-col>
+                <b-col>
+                    <h5><b>Support</b></h5>
+                    <br>
+                    <router-link to="">
+                        <h6>Help & Support</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Trust & Safety</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Selling on CloudGM</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Buying On Cloud GM</h6>
+                    </router-link>
+
+
+                </b-col>
+                <b-col>
+                    <h5><b>Community</b></h5>
+                    <br>
+                    <router-link to="">
+                        <h6>Events</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Forums</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Podcasts</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Invite Friends</h6>
+                    </router-link>
+                    <router-link to="">
+                        <h6>Become Seller</h6>
+                    </router-link>
+
+
+                </b-col>
+            </b-row>
+
+        </b-jumbotron>
     </div>
 </template>
