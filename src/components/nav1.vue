f7272cee0070ba8ad116f9b37399c828a6a7adfe
John Doe <shimijoseph94@gmail.com>
2019-08-05 14:51:13 +0530

    kkk

--- src/components/nav1.vue
+++ src/components/nav1.vue
@@ -7,7 +7,7 @@
       variant="light"
     >
       <div class="container">
-        <span>Weicome to CloudGM</span>
+        <span>Welcome to CloudGM</span>
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-auto">
@@ -81,7 +81,7 @@
     
     <b-navbar toggleable="lg" style="background-color:#f8f9fa" type="light" variant="light">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <div style="padding-left: 100px;">
+      <div style="padding-left: 50px;">
         <b-nav-item class="hv">
           <i class="fa fa-list"></i> &nbsp;CATEGORIES
         </b-nav-item>
